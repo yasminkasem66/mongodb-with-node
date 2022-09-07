e5a8da5f97644831049faf7a88ab065b3e2bb8ae
yasminkassem66 <yasmin.abdellh@gmail.com>
2022-09-07 12:35:32 +0200

    loadData

---
+++ app.js
@@ -0,0 +1,21 @@
+const MongoClient =require('mongodb').MongoClient;
+
+const circulationRepo = require('./repos/circulationRepo'); //repo
+const data = require('./circulation.json')//data
+
+const url='mongodb://localhost:27017';
+const dbName='circulation';
+
+async function main(){
+    const client= new MongoClient(url);
+    await client.connect();
+
+    const results = await circulationRepo.loadData(data);
+    console.log(results.insertedCount, results.ops);
+
+    const admin=client.db(dbName).admin();
+    // console.log(await admin.serverStatus());
+    // console.log(await admin.listDatabases());
+}
+
+main();
